d230cf4d604c913c11518cdc3f21e79ff6803c9e
Matthew B White <whitemat@uk.ibm.com>
2022-10-21 13:41:41 +0100

    Enable building of apiv1 protos
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- ci/compile_go_protos.sh
+++ ci/compile_go_protos.sh
@@ -4,16 +4,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-set -eu -o pipefail
-
-repo="build/fabric-protos-go"
-
-if [ ! -d "$repo" ]; then
-  echo "$repo does not exist"
-  exit 1
-fi
+set -x -eu -o pipefail
 
 for protos in $(find . -name '*.proto' -exec dirname {} \; | sort -u); do
-  protoc "--go_out=plugins=grpc,paths=source_relative:$repo" "$protos"/*.proto
+  protoc --go_out=bindings/go --go_opt=paths=source_relative "$protos"/*.proto
 done
 
